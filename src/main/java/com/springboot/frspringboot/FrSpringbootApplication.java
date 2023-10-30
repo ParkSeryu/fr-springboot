9b927fb5039d6de50a3d139a724ac2ee2dede797
parkseryu <pjh971212@gmail.com>
2023-10-30 15:51:18 +0900

    Initial commit

---
+++ src/main/java/com/springboot/frspringboot/FrSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.frspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/frspringboot/FrSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.frspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
