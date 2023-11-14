04508fc1676c00aa919d009548fbd4bf52f2398b
parkseryu <pjh971212@gmail.com>
2023-11-15 04:33:00 +0900

    4장 머스테치 구성 완료

---
+++ src/main/java/com/springboot/frspringboot/service/posts/PostsService.java
@@ -0,0 +1,53 @@
+package com.springboot.frspringboot.service.posts;
+
+import com.springboot.frspringboot.domain.posts.Posts;
+import com.springboot.frspringboot.domain.posts.PostsRepository;
+import com.springboot.frspringboot.web.dto.PostsListResponseDto;
+import com.springboot.frspringboot.web.dto.PostsResponseDto;
+import com.springboot.frspringboot.web.dto.PostsSaveRequestDto;
+import com.springboot.frspringboot.web.dto.PostsUpdateRequestDto;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+    }
+}
