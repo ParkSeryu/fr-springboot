6f90febe3eff0075e5490597cb018d5697f26b7f
ParkSeryu <pjh971212@gmail.com>
2023-11-28 02:00:29 +0900

    9장 CodeDeploy 연동하기!

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,7 +15,7 @@ CURRENT_PID=$(pgrep -fl aws-springboot2-webservice | grep jar | awk '{print $1}'
 
 echo "현재 구동 중인 애플리케이션 PID: $CURRENT_PID"
 
-if [ -z "$CURRENT_PID" ]; then
+if [ -n "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
 else
   echo "> kill -15 $CURRENT_PID"
@@ -37,9 +37,10 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-  -Dspring.profiles.active=real \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
 # nohup 실행 시 CodeDeploy는 무한 대기한다.
 # 이 이슈를 해결하기 위해 nohup.out 파일을 표준 입출력으로 별도 사용한다.
 # 이렇게 하지 않으면 nohup.out 파일이 생기지 않고, CodeDeploy 로그에 표준 입출력이 출력된다.
