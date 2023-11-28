937dad34f844f70e3285dead99bf4eb3f77351da
parkseryu <pjh971212@gmail.com>
2023-11-28 16:56:01 +0900

    10장 무중단 배포

--- src/main/java/com/springboot/frspringboot/config/auth/SecurityConfig.java
+++ src/main/java/com/springboot/frspringboot/config/auth/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().disable() // h2-console 화면을 사용하기 위해 해당 옵션들을 disabled.
                 .and()
                 .authorizeRequests() // URL별 권한 관리를 설정하는 옵션의 시작점이다. 이게 있어야 antMatchers 옵션을 사용할 수 있다.
-                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                 // 권한 관리 대상을 지정하는 옵션이다.
                 .antMatchers("/api/v1/**").hasRole(Role.USER.name()) // api/v1/** 주소를 가진 API는 USER 권한을 가진 사람만 가능하도록.
                 .anyRequest().authenticated() // 설정 값 이외 나머지 URL을 나타낸다.
